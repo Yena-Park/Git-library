c7f7d42a2a4320b392e7ba2b244ebdc3807b0c7a
Yena-Park <yena8317@gmail.com>
2018-09-17 00:31:29 -0400

    Comfigure Apollo Client for React and GitHub's GraphQL API

--- src/index.js
+++ src/index.js
@@ -1,123 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './style.css';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import 'cross-fetch/polyfill';
-import ApolloClient, { gql } from 'apollo-boost';
+import './style.css';
+import App from './App';
+
+const GITHUB_BASE_URL = 'https://api.github.com/graphql';
 
 require('dotenv').config();
-
-const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-  request: operation => {
-    operation.setContext({
-      headers: {
-        authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
-      },
-    });
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`
   },
 });
 
-const GET_ORGANIZATION = gql`
-  {
-    organization(login: "the-road-to-learn-react") {
-      name
-      url
-    }
-  }
-`;
+const cache = new InMemoryCache();
 
-client
-  .query({
-    query: GET_ORGANIZATION,
-  })
-  .then(console.log);
-
-const GET_ORGANIZATION_VARIABLE= gql`
-  query($organization: String!) {
-    organization(login: $organization) {
-      name
-      url
-    }
-  }
-`;
-
-client
-  .query({
-    query: GET_ORGANIZATION_VARIABLE,
-    variables: {
-      organization: 'the-road-to-learn-react',
-    },
-  })
-  .then(console.log);
-
-const GET_REPOSITORIES_OF_ORGANIZATION = gql`
-  query($organization: String!) {
-    organization(login: $organization) {
-      name
-      url
-      repositories(first: 5) {
-        edges {
-          node {
-            name
-            url
-          }
-        }
-      }
-    }
-  }
-`;
-
-client
-  .query({
-    query: GET_REPOSITORIES_OF_ORGANIZATION,
-    variables: {
-      organization: 'the-road-to-learn-react',
-    },
-  })
-  .then(console.log);
-
-const ADD_STAR = gql`
-  mutation AddStar($repositoryId: ID!) {
-    addStar(input: { starrableId: $repositoryId }) {
-      starrable {
-        id
-        viewerHasStarred
-      }
-    }
-  }
-`;
-
-client
-  .mutate({
-    mutation: ADD_STAR,
-    variables: {
-      repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
-    },
-  })
-  .then(console.log);
-
-const REMOVE_STAR = gql`
-  mutation RemoveStar($repositoryId: ID!) {
-    removeStar(input:{ starrableId: $repositoryId }) {
-      starrable {
-        id
-        viewerHasStarred
-      }
-    }
-  }
-`
-
-client
-  .mutate({
-    mutation: REMOVE_STAR,
-    variables: {
-      repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
-    },
-  })
-  .then(console.log);
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
 
 ReactDOM.render(
   <App/>, document.getElementById("root")
