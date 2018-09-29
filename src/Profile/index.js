d866c7432abd754f27b3bed76cbed61bfc411f50
Yena-Park <yena8317@gmail.com>
2018-09-29 00:31:47 -0400

    Add repositories list

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -2,6 +2,7 @@ import React , {Component} from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
+import RepositoryList from '../Repository';
 import Loading from '../Loading';
 
 const GET_CURRENT_USER = gql`
@@ -13,19 +14,51 @@ const GET_CURRENT_USER = gql`
   }
 `;
 
+const GET_REPOSITORIES_OF_CURRENT_USER = gql`
+  {
+    viewer {
+      repositories(
+        first: 5
+        orderBy: { direction: DESC, field: STARGAZERS }
+      ) {
+        edges {
+          node {
+            id
+            name
+            url
+            descriptionHTML
+            primaryLanguage {
+              name
+            }
+            owner {
+              login
+              url
+            }
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+            watchers {
+              totalCount
+            }
+            viewerSubscription
+          }
+        }
+      }
+    }
+  }
+`;
+
 const Profile = () => (
-  <Query query={GET_CURRENT_USER}>
+  <Query query={GET_REPOSITORIES_OF_CURRENT_USER}>
     {({data, loading}) => {
-      const {viewer} = data;
+      const { viewer } = data;
 
       if(loading || !viewer) {
-        return <Loading/>;
+        return <Loading />;
       }
-      return (
-        <div>
-          {viewer.name} {viewer.login}
-        </div>
-      );
+
+      return <RepositoryList repositories={viewer.repositories} />;
     }}
   </Query>
 );
